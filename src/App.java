6e51042451012c539e1a91318159189d24d9e58e
wooseokoh <allsafe1206@gmail.com>
2022-10-28 23:29:58 +0900

    02. proxy pattern

--- src/App.java
+++ src/App.java
@@ -1,12 +1,13 @@
-import ex01.Cat;
-import ex01.DoorMan;
-import ex01.Mouse;
+import ex02.Cat;
+import ex02.DoorMan;
+import ex02.Mouse;
+import ex02.DoorManProxy;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Mouse m = new Mouse();
         Cat c = new Cat();
-        DoorMan dm = new DoorMan();
+        DoorManProxy dm = new DoorManProxy(new DoorMan());
         dm.쫒아내(m);
         dm.쫒아내(c);
     }
---
+++ src/ex02/DoorMan.java
@@ -0,0 +1,7 @@
+package ex02;
+
+public class DoorMan {
+    public void 쫒아내(Animal a) {
+        System.out.println(a.getName() + " 쫒아내");
+    }
+}
